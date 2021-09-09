aaf1bd3fa5c7b3d4c78e7c338804cc1bda35b975
Ahsan-Ehtesham <ahsanehtesham110@gmail.com>
2021-09-09 06:43:04 +0500

    Loader Updated

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,14 +1,16 @@
-import React, { Component } from 'react'
-import loading from '../loader.gif'
+import React, { Component } from "react";
 
 export class Spinner extends Component {
-    render() {
-        return (
-            <div className="text-center">
-                <img src={loading} alt="loading"></img>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="d-flex justify-content-center">
+        <div class="ripple-loader">
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Spinner
+export default Spinner;
